dd62f6ae0309825654b1c383171d470fd6ac159c
Christophe Prud'homme <christophe.prudhomme@cemosis.fr>
2025-09-14 15:49:12 +0200

    refactor: Update netlify build script to use npm run commands for better clarity and consistency

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 npm i
-npx gulp bundle
-npx gulp preview:build
-npx antora --html-url-extension-style=indexify site.yml
+npm run build:ui
+npm run build:site
+npm run build:search
